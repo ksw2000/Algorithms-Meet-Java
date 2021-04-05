40e56029962ac8bac167b832381f76cc5ba350a8
VJ <twjmy@msn.com>
2021-04-06 06:47:14 +0800

    Modify PCL4107056019.java
    Changes:
    1. Function composition; (take class HashMap apart)
    2. Let the main thread participates the calculate;
    3. Daemon threading; (end up all threads while return)
    4. Threading by frequency instead of calculating variable start and end before launch;
    5. Change variable names more close to the data structure for reading; (object got capital first letter and constant got all capital)
    6. End up the thread instead of putting the result variable to the break condition;
    7. More end up judge. (after launch and before join each thread)
    8. More constant, more comment.

--- Homework/HW04_PCL/PCL4107056019.java
+++ Homework/HW04_PCL/PCL4107056019.java
@@ -1,93 +1,80 @@
 public class PCL4107056019 extends PCL{
-    volatile boolean ans;
-    int tNum = 32;
-    int lgTNum = 5;
-    Thread[] t = new Thread[tNum];
+	class Slot{ // class for saving slope of the pair as linked list
+		final double slope; final Slot Next;
+		public Slot(final double slope, final Slot Next)
+		{ this.slope = slope; this.Next = Next; }
+	}
+	final byte CORE = 5; // Binary logarithm of launch thread number
+	final Slot[][] HashTable = new Slot[1<<CORE][]; // Initialize 2^5=32 hash table
+	final Thread[] T = new Thread[HashTable.length-1]; // Initialize 32-1=31 threads, one for main thread
+	volatile boolean result; // the result vaiue. modifier volatile let us not to care synchronous problem
 
     public boolean checkPCL(int[][] array){
-        ans = false;
-        int len = array.length;
-        int hashCap = 1 << ((int)Math.ceil(Math.log10((double)len) / 0.3010));
+		result = false; // reset the value, prevent same object-method calling
 
-        if(len > tNum){
-            for(int k=0; k<tNum; k++){
-                final int finalK = k;
-                t[k] = new Thread(new Runnable(){
-                    public void run(){
-                        final int start = (len * finalK)     >> lgTNum;
-                        final int end   = (len * (finalK+1)) >> lgTNum;
-                        HashMap m = new HashMap(hashCap);
+		final int AEND = A.length-1, TLEN = T.length, FREQ = HashTable.length,
+				// get the closest power-of-2-number bigger than A.length, BUCKETS = 2^k, k in [0,32]
+				BUCKETNUM = 1<<32-Integer.numberOfLeadingZeros(AEND), B = BUCKETNUM-1;
 
-                        for(int i=start; i<end && !ans; i++){
-                            for(int j=i+1; j<len && !ans; j++){
-                                if(m.containOrPut((double)(array[i][1] - array[j][1]) / (double)(array[i][0] - array[j][0]))){
-                                    ans = true;
-                                }
-                            }
-                            m.reset();
-                        }
-                    }
-                });
-                t[k].start();
-            }
+		for(byte ti=0; ti<TLEN; ++ti){ // start up all threads
+			final byte t = ti; // the variables pass to anonymous class must be final
+			T[t] = new Thread(()->{ // lambda expression for Runnable interface since Java 8
+				// start of each thread(algorithm same as below)
+				// threading by frequency: e.g. 0,32,64... 1,33,65... 2,34,66... ......
+				for(int i=AEND-t, j, bucket; i>-1; i-=FREQ){
+					// (re-)set buckets for each i
+					for(HashTable[t] = new Slot[BUCKETNUM], j=i-1; j>-1;){
+						// calculate the slope of the line construct by point i and j
+						final double slope = (A[i][1]-A[j][1])/(double)(A[i][0]-A[j--][0]);
 
-            try{
-                for(Thread v : t) v.join();
-            }catch(InterruptedException e){}
+						// modulo for ONLY power-of-2-number: same as (hashcode&0x7fff_ffff)%BUCKETNUM
+						// for keep the hash index in [0,BUCKETMUN]
+						bucket = Double.hashCode(slope)&B&0x7fff_ffff;
 
-            return ans;
-        }else{
-            int i,j;
-            HashMap m = new HashMap(hashCap);
-            for(i=0; i<len; i++){
-                for(j=i+1; j<len; j++){
-                    if(m.containOrPut((double)(array[i][1] - array[j][1]) / (double)(array[i][0] - array[j][0]))){
-                        return true;
-                    }
-                }
-                m.reset();
-            }
+						// view through slots of the bucket, hash collisions occur if Solt have Next.
+						for(Slot Pivot = HashTable[t][bucket]; Pivot != null; Pivot = Pivot.Next){
+							// find three points in the same line
+							if(Pivot.slope == slope) result = true;
 
-            return false;
-        }
+							// if other thread(s) or this thread found, end up the thread.
+							// the same instruction below is for the same reason.
+							if(result) return;
+						}
 
-    }
+						// put slope into the foremost slot of the bucket
+						HashTable[t][bucket] = new Slot(slope, HashTable[t][bucket]);
 
-    private class HashMap{
-        class Entry{
-            public double key;
-            public Entry next;
-        }
+						if(result) return;
+					}
+					if(result) return;
+				}
+				// end of each thread
+			});
+			T[t].setDaemon(true); // let threads end up while main thread return ture
+			T[t].start(); // launch threads
+			if(result) return true;
+		} // end of start up threads
 
-        private int cap;
-        private Entry[] list;
+		// start algorithm to main thread
+		for(int i=AEND-TLEN, j, bucket; i>-1; i-=FREQ){
+			for(HashTable[TLEN] = new Slot[BUCKETNUM], j=i-1; j>-1;){
+				final double slope = (A[i][1]-A[j][1])/(double)(A[i][0]-A[j--][0]);
+				bucket = Double.hashCode(slope)&B&0x7fff_ffff;
+				for(Slot Pivot = HashTable[TLEN][bucket]; Pivot != null; Pivot = Pivot.Next)
+					if(Pivot.slope == slope || result)
+						return true;
+				HashTable[TLEN][bucket] = new Slot(slope, HashTable[TLEN][bucket]);
+			}
+			if(result) return true;
+		}
+		// end of algorithm in main thread
 
-        HashMap(int size){
-            this.cap = size;
-            Entry[] newEntry = new Entry[this.cap];
-            this.list = newEntry;
-        }
-
-        final public void reset(){
-            Entry[] newEntry = new Entry[this.cap];
-            this.list = newEntry;
-        }
-
-        final public boolean containOrPut(double key){
-            // & (this.cap - 1) is equivalent to % this.cap when base of this.cap is 2
-            int index = (Double.valueOf(key).hashCode() & 0x7fffffff) & (this.cap-1);
-
-            for(Entry current = list[index]; current!=null; current=current.next){
-                if(current.key == key){
-                    return true;
-                }
-            }
-
-            Entry newEntry = new Entry();
-            newEntry.key  = key;
-            newEntry.next = list[index];
-            list[index] = newEntry;
-            return false;
-        }
+		try{
+			for(final Thread t : T){ // join all threads
+				if(result) return true;
+				t.join();
+			}
+		}catch(final InterruptedException e){}
+		return result;
     }
 }
